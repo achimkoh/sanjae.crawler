d0d2d21cd949f44ca860e7144823b12abca97b25
Wonil <red112@naver.com>
2021-05-03 11:21:59 +0900

    산재 판례 데이터 추출 코드 등록

---
+++ Extract_details_From_OpenData/Extract_sentence_details.py
@@ -0,0 +1,39 @@
+'''
+Code for Korea - #p-산업재해
+판례 데이터 분석을 위한 판례 데이터 덤프 생성 코드
+2021/05/03 바른생활 심원일
+'''
+
+import API_Industrial_disasters as API
+from pandas import DataFrame    # 코드 없이 데이터 분석을 용이하게 할 수 있도록 엑셀로 저장
+import datetime                 # 저장 파일 이름에 날짜 사용
+
+#### 조회를 위한 세 가지 유형 정보들 조회
+# 판결 유형 조회
+KindA_precedent_result_type_list = API.get_KindA_PrecedentResult_types()
+print(KindA_precedent_result_type_list)
+
+# 사건 유형 조회
+KindB_case_type_list = API.get_KindB_case_types()
+print(KindB_case_type_list)
+
+# 사고/질병 구분 조회
+KindC_AccidentDisease_type_list = API.get_KindC_AccidentDisease_types()
+print(KindC_AccidentDisease_type_list)
+
+#### 유형별 사건 개수 조회
+counts_for_Cases = API.get_Types_counts(KindA_precedent_result_type_list,KindB_case_type_list,KindC_AccidentDisease_type_list)
+# 조회 된 정보 엑셀로 저장
+df = DataFrame(counts_for_Cases,columns=['판결유형','사건유형','사고질병 구분','개수'])
+today = datetime.datetime.today()
+filename = '유형별_사건_개수_'+str(today.year)+'_'+str(today.month)+'_'+str(today.day)+'.xlsx'
+df.to_excel(filename,index=False, header=True)
+
+#### 사건 상세 내용 조회
+details_for_Cases = API.get_detials_for_Cases(counts_for_Cases)
+
+df = DataFrame(details_for_Cases,columns=['사건번호','법원','판결유형','사건유형','사고질병 구분','판결문','제목','연관사건'])
+filename = '사건상세_'+str(today.year)+'_'+str(today.month)+'_'+str(today.day)+'.xlsx'
+df.to_excel(filename,index=False, header=True)
+
+print("---------------------- 출력 완료 -------------------------")
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Industrial_disasters
 산업재해 관련 프로젝트 - 판례 분석 등
+
+#  산재 판례 데이터 수집
+https://github.com/Code-for-Korea/Industrial_disasters
+근로복지공단_산재보험 판례 판결문 조회 서비스 데이터 활용
+
+- 사용법
+ 1. API_Industrial_disasters.py 에 발급받은 라이센스 키(Encoded)를 설정함.
+ 2. Extract_sentence_details.py 를 실행함
+ 3. 유형별 사건 개수와 각 사건 상세 내용이 기록 된 엑셀 파일이 2개 생성 됨.
+ 4. 연관 재판 정보는 상세 내용 파일의 마지막 컬럼을 엑셀의 데이터 분리 기능으로 쉼표와 빼기를 구분자로 하여 나누면 됨.
+ 
